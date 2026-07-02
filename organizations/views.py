e0ee5085dd80d79cbce54dc289910bb408bd5442
Anjali470 <anjalikonkala001@gmail.com>
2026-07-02 13:03:54 +0530

    feat(auth): add custom organization owner permission

--- organizations/views.py
+++ organizations/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from common.permissions import IsOrganizationOwner
 
 from .models import Organization
 from .serializers import OrganizationSerializer
@@ -18,7 +19,7 @@ class OrganizationListCreateView(generics.ListCreateAPIView):
 
 class OrganizationDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrganizationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOrganizationOwner]
 
     def get_queryset(self):
         return Organization.objects.filter(owner=self.request.user)
